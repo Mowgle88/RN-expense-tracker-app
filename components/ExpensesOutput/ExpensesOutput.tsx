853f798e9d65ef6f5b9e723927ff03e5ad1ee9fb
Mowgle88 <mowgle1988@gmail.com>
2022-08-28 14:42:02 +0300

    feat: improve app Layout and Styling

--- components/ExpensesOutput/ExpensesOutput.tsx
+++ components/ExpensesOutput/ExpensesOutput.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import ExpensesSummary from './ExpensesSummary';
 import ExpensesList from './ExpensesList';
 import { IDummyExpenses } from '../../constants/dummyExpenses';
+import { GlobalStyles } from '../../constants/style';
 
 interface ExpensesOutputProps {
   expenses: IDummyExpenses[],
@@ -11,11 +12,17 @@ interface ExpensesOutputProps {
 
 export default function ExpensesOutput({ expenses, expensesPeriod }: ExpensesOutputProps) {
   return (
-    <View>
+    <View style={styles.container}>
       <ExpensesSummary expenses={expenses} periodName={expensesPeriod} />
       <ExpensesList expenses={expenses} />
     </View>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 24,
+    backgroundColor: GlobalStyles.colors.primary700,
+  },
+})
--- components/ExpensesOutput/ExpensesSummary.tsx
+++ components/ExpensesOutput/ExpensesSummary.tsx
@@ -1,6 +1,7 @@
 import { StyleSheet, Text, View } from 'react-native';
 import React from 'react';
 import { IDummyExpenses } from '../../constants/dummyExpenses';
+import { GlobalStyles } from '../../constants/style';
 
 interface ExpensesSummaryProps {
   expenses: IDummyExpenses[],
@@ -14,11 +15,29 @@ export default function ExpensesSummary({ expenses, periodName }: ExpensesSummar
   }, 0);
 
   return (
-    <View>
-      <Text>{periodName}</Text>
-      <Text>${expenseSum.toFixed(2)}</Text>
+    <View style={styles.container}>
+      <Text style={styles.period}>{periodName}</Text>
+      <Text style={styles.sum}>${expenseSum.toFixed(2)}</Text>
     </View>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    padding: 8,
+    backgroundColor: GlobalStyles.colors.primary50,
+    borderRadius: 6,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  },
+  period: {
+    fontSize: 12,
+    color: GlobalStyles.colors.primary400
+  },
+  sum: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: GlobalStyles.colors.primary500
+  }
+})
