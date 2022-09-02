d020c40b1d5f77056a0dc3d6206944f3f55f99f5
Mowgle <84435130+Mowgle88@users.noreply.github.com>
2022-09-02 22:46:33 +0300

    feat: add validation

--- components/ManageExpense/ExpenseForm.tsx
+++ components/ManageExpense/ExpenseForm.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Alert, StyleSheet, Text, View } from 'react-native'
 import React, { useState } from 'react'
 import CustomInput from './CustomInput'
 import CustomButton from '../UI/CustomButton';
@@ -34,6 +34,16 @@ export default function ExpenseForm(this: any, { onCancel, onSubmit, submitButto
       date: new Date(inputValue.date),
       description: inputValue.description
     }
+
+    const amountIsValid = !isNaN(expenseData.amount) && expenseData.amount > 0;
+    const dateIsValid = expenseData.date.toString() !== 'Invalid Date';
+    const descriptionIsValid = expenseData.description.trim().length > 0;
+
+    if (!amountIsValid || !dateIsValid || !descriptionIsValid) {
+      Alert.alert('Invalid input', 'Please check your input values')
+      return
+    }
+
     onSubmit(expenseData);
   }
 
