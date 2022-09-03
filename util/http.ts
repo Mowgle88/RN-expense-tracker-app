587a4cd677dcb1fd3cf638c26a06cbf38083ecba
Mowgle <84435130+Mowgle88@users.noreply.github.com>
2022-09-03 19:37:59 +0300

    feat: add function sending GET Http requests

--- util/http.ts
+++ util/http.ts
@@ -1,6 +1,26 @@
 import axios from "axios";
 import { IDummyExpenses } from "../constants/dummyExpenses";
 
+const BACKEND_URL = 'https://rn-expense-tracker-app-default-rtdb.firebaseio.com';
+
 export function storeExpenses(expenseData: IDummyExpenses) {
-  axios.post('https://rn-expense-tracker-app-default-rtdb.firebaseio.com/expenses.json', expenseData)
+  axios.post(`${BACKEND_URL}/expenses.json`, expenseData)
+}
+
+export async function fetchExpenses() {
+  const response = await axios.get(`${BACKEND_URL}/expenses.json`);
+
+  const expenses = [];
+
+  for (const key in response.data) {
+    const expenseObj = {
+      id: key,
+      amount: response.data[key].amount,
+      date: new Date(response.data[key].date),
+      description: response.data[key].description,
+    };
+    expenses.push(expenseObj);
+  }
+
+  return expenses;
 }
